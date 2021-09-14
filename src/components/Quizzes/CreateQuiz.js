ea3dcb83823b5e4185d02b689b5ea89814c138a8
Cameron White <cameronpwhite98@gmail.com>
2021-09-14 14:34:36 -0500

    tweaks to create quiz form

--- src/components/Quizzes/CreateQuiz.js
+++ src/components/Quizzes/CreateQuiz.js
@@ -4,125 +4,153 @@ import React, { useState, useEffect } from 'react'
 
 export const CreateQuiz = () => {
 
+    const [quizData, updateQuiz] = useState({
+        quizName: "",
+        categoryId: 0,
+        userId: 0
+    })
+
+    const handleQuizSubmit = (e) => {
+        e.preventDefault()
+
+        const newQuiz = {
+            quizName: `${quizData.quizName}`,
+            categoryId: quizData.categoryId,
+            userId: quizData.userId
+        }
+
+    }
+
+    const handleQuizInput = (e) => {
+
+        const copiedData = { ...quizData }
+        copiedData[e.target.id] = e.target.value
+        updateQuiz(copiedData)
+
+    }
+
+
     return (
         <>
             <h1>Create a Quiz</h1>
-            <div className="quiz-form"></div>
-            <label htmlFor="quizName">Quiz Name</label>
-            <input
-                type="text"
-                required
-                autoFocus
-                className="form-control"
-                id="quizName"
-                placeholder="Quiz name"
-            />
-            {/* Generate multiple questions */}
-            <h2>Question</h2>
-            <div className='form-questionContent'>
-                <label htmlFor="questionContent">Question:  </label>
+            <form>
+                <div className="quiz-form"></div>
+                <label htmlFor="quizName">Quiz Name</label>
                 <input
                     type="text"
                     required
+                    autoFocus
                     className="form-control"
                     id="quizName"
-                    placeholder="Question"
+                    placeholder="Quiz name"
                 />
-            </div>
-            <label htmlFor="selectionType">Selection Type:  </label>
-            <label htmlFor="multipleChoice">Multiple Choice</label>
-            <input
-                type="radio"
-                required
-                className="form-control"
-                id="multipleChoice"
-                placeholder="Selection Type"
-                checked
-            />
-            <label htmlFor="trueFalse">True/False</label>
-            <input
-                type="radio"
-                required
-                className="form-control"
-                id="trueFalse"
-                placeholder="Selection Type"
-            />
-            <label htmlFor="fillinBlank">Fill-in Blank</label>
-            <input
-                type="radio"
-                required
-                className="form-control"
-                id="fillinBlank"
-                placeholder="Selection Type"
-            />
-            <div className="form-possibleAnswers">
-                <label htmlFor="possibleAnswers">Possible Answers:</label>
-            <input
-                type="text"
-                required
-                className="form-control"
-                id="possibleAnswer"
-                placeholder="Answer 1"
-            />
-            <input
-                type="text"
-                required
-                className="form-control"
-                id="quizName"
-                placeholder="Answer 2"
-            />
-            <input
-                type="text"
-                required
-                className="form-control"
-                id="quizName"
-                placeholder="Answer 3"
-            />
-            <input
-                type="text"
-                required
-                className="form-control"
-                id="quizName"
-                placeholder="Answer 4"
-            />
-            </div>
-            <div className="form-category">
-                <label htmlFor="categories">Category:</label>
-                <label htmlFor="categoryHistory">History</label>
+                {/* Generate multiple questions */}
+                <h2>Question</h2>
+                <div className='form-questionContent'>
+                    <label htmlFor="questionContent">Question:  </label>
+                    <input
+                        type="text"
+                        required
+                        className="form-control"
+                        id="quizName"
+                        placeholder="Question"
+                    />
+                </div>
+                <label htmlFor="selectionType">Selection Type:  </label>
+                <label htmlFor="multipleChoice">Multiple Choice</label>
                 <input
-                type="radio"
-                required
-                className="form-control"
-                id="categoryHistory"
-                placeholder="Category"
-                checked
+                    type="radio"
+                    required
+                    className="form-control"
+                    id="multipleChoice"
+                    placeholder="Selection Type"
+                    checked
                 />
-                <label htmlFor="categoryScience">Science</label>
+                <label htmlFor="trueFalse">True/False</label>
                 <input
-                type="radio"
-                required
-                className="form-control"
-                id="categoryScience"
-                placeholder="Category"
+                    type="radio"
+                    required
+                    className="form-control"
+                    id="trueFalse"
+                    placeholder="Selection Type"
                 />
-                <label htmlFor="categoryMath">Math</label>
+                <label htmlFor="fillinBlank">Fill-in Blank</label>
                 <input
-                type="radio"
-                required
-                className="form-control"
-                id="categoryMath"
-                placeholder="Category"
+                    type="radio"
+                    required
+                    className="form-control"
+                    id="fillinBlank"
+                    placeholder="Selection Type"
                 />
-            </div>
-            <div className="addQuestion">
-                <button>Add Question</button>
-            </div>
-            <div className="submitButton">
-                <button>Submit Quiz</button>
-            </div>
-            <div className="cancelButton">
-                <button>Cancel</button>
-            </div>
+                <div className="form-possibleAnswers">
+                    <label htmlFor="possibleAnswers">Possible Answers:</label>
+                    <input
+                        type="text"
+                        required
+                        className="form-control"
+                        id="possibleAnswer"
+                        placeholder="Answer 1"
+                    />
+                    <input
+                        type="text"
+                        required
+                        className="form-control"
+                        id="quizName"
+                        placeholder="Answer 2"
+                    />
+                    <input
+                        type="text"
+                        required
+                        className="form-control"
+                        id="quizName"
+                        placeholder="Answer 3"
+                    />
+                    <input
+                        type="text"
+                        required
+                        className="form-control"
+                        id="quizName"
+                        placeholder="Answer 4"
+                    />
+                </div>
+                <div className="form-category">
+                    <label htmlFor="categories">Category:</label>
+                    <label htmlFor="categoryHistory">History</label>
+                    <input
+                        type="radio"
+                        required
+                        className="form-control"
+                        id="categoryHistory"
+                        placeholder="Category"
+                        checked
+                    />
+                    <label htmlFor="categoryScience">Science</label>
+                    <input
+                        type="radio"
+                        required
+                        className="form-control"
+                        id="categoryScience"
+                        placeholder="Category"
+                    />
+                    <label htmlFor="categoryMath">Math</label>
+                    <input
+                        type="radio"
+                        required
+                        className="form-control"
+                        id="categoryMath"
+                        placeholder="Category"
+                    />
+                </div>
+                <div className="addQuestion">
+                    <button>Add Question</button>
+                </div>
+                <div className="submitButton">
+                    <button>Submit Quiz</button>
+                </div>
+                <div className="cancelButton">
+                    <button>Cancel</button>
+                </div>
+            </form>
         </>
     )
 }
